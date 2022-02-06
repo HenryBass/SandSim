2c09925adc673d9f9d38f38adb09ea085a82237c
henry <63601449+HenryBass@users.noreply.github.com>
2022-02-06 15:55:29 +0000

    things are working less

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 class Air {
   constructor() {
+    this.updated = false;
+    this.solid = false;
     this.type = "air";
     this.mass = 0;
     var multval = 55;
@@ -8,7 +10,7 @@ class Air {
     this.g = Math.abs(200 + colormult);
     this.b = Math.abs(200 + colormult);
   }
-  update(x, y, map) {
+  update(x, y, map, nextmap) {
     var nextmap = map;
     return nextmap;
   }
@@ -16,6 +18,8 @@ class Air {
 
 class Sand {
   constructor() {
+    this.updated = false;
+    this.solid = true;
     this.type = "sand";
     this.mass = 2;
     var multval = 55;
@@ -24,50 +28,76 @@ class Sand {
     this.g = Math.abs(150 + colormult);
     this.b = Math.abs(0 + colormult);
   }
-  update(x, y, map) {
+  update(x, y, map, nextmap) {
     var nextmap = map;
+    return nextmap;
+  }
+}
 
-    var below = nextmap[x][y+1];
-    if (below != undefined) {
-      if (below.mass < self.mass) {
-        nextmap[x][y+1] = self;
-        nextmap[x][y] = below;
-      } else if (below.mass > self.mass) {
-          nextmap[x][y-1] = self;
-          nextmap[x][y+2] = below;
-      }
-    }
+class Stone {
+  constructor() {
+    this.updated = false;
+    this.solid = true;
+    this.type = "sand";
+    this.mass = 10;
+    var multval = 20;
+    var colormult = Math.round((Math.random() * multval) - (multval / 2));
+    this.r = Math.abs(50 + colormult);
+    this.g = Math.abs(50 + colormult);
+    this.b = Math.abs(50 + colormult);
+  }
+  update(x, y, map, nextmap) {
+    var nextmap = map;
     return nextmap;
   }
 }
 
 class Water {
   constructor() {
+    this.updated = false;
+    this.solid = false;
     this.type = "water";
     this.mass = 1;
     var multval = 55;
     var colormult = Math.round((Math.random() * multval) - (multval / 2));
     this.r = Math.abs(0 + colormult);
-    this.g = Math.abs(0 + colormult);
+    this.g = Math.abs(10 + colormult);
     this.b = Math.abs(200 + colormult);
   }
-  update(x, y, map, self) {
-    var nextmap = map;
+  update(x, y, map, self, nextmap) {
 
     var below = nextmap[x][y+1];
-    if (below != undefined) {
-      if (below.mass < self.mass) {
+    if ((below != undefined) && (below.mass < self.mass) && (below.solid = true)) {
         nextmap[x][y+1] = self;
         nextmap[x][y] = below;
-      } else if (below.mass > self.mass) {
-          nextmap[x][y-1] = self;
-          nextmap[x][y+2] = below;
+      } else {
+        var r = Math.round((Math.random() * 2) - 1);
+        var nextpos = nextmap[x + r][y];
+        var defined = (nextpos != undefined)
+        if ((defined) && (nextpos.solid == false)) {
+          nextmap[x + r][y] = self;
+          nextmap[x][y] = nextpos;
+        }
       }
-    }
+    
+
+    this.updated = true;
     return nextmap;
   }
 }
 
+function shuffle(array)
+{
+  var m = array.length, t, i;
+  while (m > 0) 
+  {
+	i = Math.floor(Math.random() * m--);
+	t = array[m];
+	array[m] = array[i];
+	array[i] = t;
+  }
+  return array;
+}
 
 function setup() {
   const width = canvaswidth;
@@ -116,7 +146,9 @@ function draw() {
   while (y < height) {
     while (x < width) {
       px = pxs[x][y];
-      nextpxs = px.update(x, y, pxs, px);
+      if (px.updated == false) {
+        nextpxs = px.update(x, y, pxs, px, nextpxs);
+      }
       x++;
     }
     x = 0;
@@ -128,6 +160,7 @@ function draw() {
 while (y < height) {
   while (x < width) {
     px = nextpxs[x][y];
+    px.updated = false;
     fill(px.r, px.g, px.b);
     rect(x * scalex, y * scaley, scalex, scaley);
     x++;
@@ -140,13 +173,13 @@ y = 0;
 }
 
 function place() {
-  if((mouseX < canvaswidth) && (mouseY < canvasheight)) {
-      if(mouseButton === LEFT) {
-        var placepx = new Sand();
-      } else if (mouseButton === RIGHT) {
-        var placepx = new Water();
-      }
-      pxs[Math.round(mouseX / scalex)][Math.round(mouseY / scaley)] = placepx;
+  
+   var blocktype = document.getElementById("blocks").value; 
+    try  {
+    eval("var placepx = new " + str(blocktype) + "();")
+    pxs[Math.round(mouseX / scalex)][Math.round(mouseY / scaley)] = placepx;
+  } catch {
+    0;
   }
 }
 
